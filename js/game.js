b795f03f31a0cfec7db2935e152203997c6771cf
Dave Menninger <dave.menninger@gmail.com>
2018-04-29 23:12:09 -0400

    initial commit

---
+++ js/game.js
@@ -0,0 +1,52 @@
+console.log('hello');
+
+var Game = {};
+
+Game.init = function(){
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function(){
+    game.load.tilemap('map','assets/map/time-hole.json',null,Phaser.Tilemap.TILED_JSON );
+    game.load.spritesheet('tileset','assets/map/tilesheet.png',32,32);
+    game.load.image('sprite','assets/sprites/sprite.png');
+};
+
+Game.create = function(){
+    Game.playerMap = {};
+    var map = game.add.tilemap('map');
+    map.addTilesetImage('tilesheet','tileset');
+    var layer;
+    for ( var i = 0; i < map.layers.length ; i++ )
+    {
+        layer = map.createLayer(i);
+    }
+    layer.inputEnabled = true;
+    layer.events.onInputUp.add(Game.getCoordinates, this);
+};
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
+
+Game.getCoordinates = function(layer,pointer){
+    console.log(pointer.worldX+' '+pointer.worldY);
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var duration = distance*10;
+    var tween = game.add.tween(player);
+    tween.to({x:x,y:y},duration);
+    tween.start();
+};
+
+var game = new Phaser.Game(24*32,17*32,Phaser.AUTO,document.getElementById('game'));
+game.state.add('Game',Game);
+game.state.start('Game');
